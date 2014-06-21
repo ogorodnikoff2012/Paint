95ee4f53bb3e99e197c9b77a3c73eb724bf94e00
Vladimir Ogorodnikov <ogorodnikoff2012@yandex.ru>
2014-06-21 22:16:24 +0400

    Fool protection

--- Core/main.cpp
+++ Core/main.cpp
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
 
     MainWindow* window = new MainWindow;
     window->show();
+	window->newFile();
 
     //qDebug() << QImageWriter::supportedImageFormats();
     return a.exec();
